a9b6c9b75823f3a16b4652d076b6e1c3e4aee2e9
Dmytro Litvinov <DLitvinov@ARM.DOMAIN>
2024-01-14 09:42:17 -0600

    feat: add generics example

---
+++ Essential2.Library/Sorter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Essential2.Library
+{
+    public class Sorter<T> where T: class, IComparable<T>, new()
+    {
+        public void Sort(T[] items) { 
+            for (int i = 1; i < items.Length; i++) { 
+                if (items[i].CompareTo(items[i - 1]) < 0) {
+                    Swap(items, i - 1, i);
+                }
+            }
+        }
+
+        private void Swap<T>(T[] items, int first, int second)
+        {
+            T temp = items[second];
+            items[second] = items[first];
+            items[first] = temp;
+        }
+    }
+}
